7aab8631ee1ebd331b76079a9106a881e783b788
Mikhail Klimentov <mikhail.klimentov@gmail.com>
2016-08-24 20:48:30 -0400

    Spacebar commit brought up to most recent 1.0.0 upstream commit

--- demo/demo.js
+++ demo/demo.js
@@ -115,7 +115,6 @@ export default class Demo extends Component {
                   steps={[0, 4, 8, 12, 16, 20, 24, 28]}
                 />
               </Compressor>
-            </Sequencer>
           </Analyser>
         </Song>
 
